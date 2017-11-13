c047b20e72211f5cd915d1bdff34810041fa804f
renanbastos93 <renanbastos.tec@gmail.com>
2017-11-13 18:19:03 +0000

    [+] base to PWA with angularjs and ngMap

---
+++ js/main.js
@@ -0,0 +1,46 @@
+angular.module('hhV2', ['ngRoute', 'ngMap']) //ngMap
+.controller('mapBlood', function($scope, $http, NgMap){ //NgMap
+    $scope.teste = "asuhdahudauh";
+    // $scope.markers = [
+    //     {
+    //         "clinicas": "-30.0442021,-51.2077004"
+    //     },
+    //     {
+    //         "pucrs": "-30.0566752,-51.1751618"
+    //     },
+    //     {"hemocentro-bento": "-30.046577,-51.2175465"},
+    //     {"santa casa": "-30.046577,-51.2175465"}
+    // ]
+
+    $http({
+        method: "GET",
+        url: "https://hemoheroes.com/api/user/bank"
+    }).then(
+        (success) => {
+            $scope.markers = success.data.results;
+        }
+    ).catch(
+        (err) => {
+            console.log(err)
+        }
+    )
+
+    NgMap.getMap().then(function(map) { 
+        // console.log(map.getCenter());
+        // console.log('markers', map.markers);
+        // console.log('shapes', map.shapes);
+      });
+
+})
+.config(function($routeProvider)
+    {
+        $routeProvider
+        .when("/", {
+            templateUrl : "templates/map.html",
+            controller  : 'mapBlood'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
+    }
+);
---
+++ sw.js
@@ -0,0 +1,72 @@
+const VERSION = 'hemoheroes@v2-'+new Date().getMilliseconds();
+
+self.addEventListener('install', event => {
+    event.waitUntil(new Promise((resolve, reject) => {
+    	caches.open(VERSION).then(cache => {
+    		return cache.addAll([
+                '/',
+                '/index.html',
+                '/templates/map.html',
+                '/404.html',
+                '/css/style.css',
+                '/js/main.js',
+                '/bower_components/angular/angular.min.js',
+                '/bower_components/angular-route/angular-route.min.js',
+                '/bower_components/ngmap/build/scripts/ng-map.min.js'
+    		]).then(_ => {
+    			console.log('INSTALLED ' + VERSION);
+        		resolve();
+    		}).catch(err => {
+    			console.erro('Não deu!', err);
+    		})
+    	})
+    }))
+})
+
+self.addEventListener('activate', event => {
+    event.waitUntil(new Promise((resolve, reject) => {
+        caches.keys().then(keysList => {
+            return Promise.all(keysList.map(cacheKey => {
+                if (cacheKey !== VERSION) {
+                    return caches.delete(cacheKey)
+                }
+            })).then(_ => {
+                console.log('ACTIVATED ' + VERSION)
+                resolve()
+            })
+        })
+    }))
+})
+
+self.addEventListener('fetch', event => {
+    const url = new URL(event.request.url)
+    const errorPage = './404.html'
+    console.log('Requisitou: ', event.request.url)
+
+    return event.respondWith(
+        caches.match(event.request).then(response => {
+        	return response || fetch(event.request).then(response => {
+        		if (response.ok) {
+        			caches.open(VERSION).then(cache => {
+        				cache.put(event.request, response)
+        			})
+        			return response.clone()
+        		} else {
+        			// return caches.match(errorPage)
+        		}
+        	})
+        }).catch(
+            x => {
+                console.log("X", x)
+                // return caches.match(errorPage)
+            }
+        )
+    )
+});
+
+
+
+
+
+
+
