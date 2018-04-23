3951441a5ef43aa5b40a6486dd3ab16ebb8f2530
renanbastos93 <renan.bastos@unirede.net>
2018-04-23 14:33:03 +0000

    improvement notification to first habilit and ofline

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -2,16 +2,17 @@
     "use strict";
     
     function notifique(){
-        var op = {
-            body: 'Você irá receber as noticações desse jeito!', 
-            icon:'./assets/images/Logo-email.png'
-        };
-        var title = "HemoHeroes Notifica";
-        var offline = {
-            body: "Olá, você está offline. assim que tiver online acesse novamente!",
-            icon:'./assets/images/Logo-email.png'
-        };
         if('Notification' in window){
+
+            var op = {
+                body: 'Você irá receber as noticações desse jeito!', 
+                icon:'./assets/images/Logo-email.png'
+            };
+            var title = "HemoHeroes Notifica";
+            var offline = {
+                body: "Olá, você está offline. assim que tiver online acesse novamente!",
+                icon:'./assets/images/Logo-email.png'
+            };
             
             if(Notification.permission === 'granted'){
                 if(!navigator.onLine) {
@@ -28,6 +29,7 @@
             }
 
         }
+        return ('Notification' in window);
     };
     
     notifique();
