95b6be0f8e629201f091b598b68d1e97b1026acd
renanbastos93 <renan.bastos@unirede.net>
2018-04-23 14:32:37 +0000

    ajustado validação de cadastro

--- assets/js/http.js
+++ assets/js/http.js
@@ -1,110 +1,123 @@
 "use strict";
 
-const urlBase = "https://hemoheros-api.herokuapp.com/api/v1/";
+const urlBase = "./api/v1/";
 
 let post = (url, param) => fetch(url, param).then(response => response.json());
-
-const register = (who) => {
-    let url = urlBase + who;
-    let data = validData();
-    let options = {
-        body: JSON.stringify(data),
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        }
-    };
-    if(data != null) {
-        post(url, options)
-        .then(data => console.log(data))
-        .catch(err => console.log("err ", err))
-    }
-};
-
-const validData = () => {
-    let data = [];
-    let validated = {};
-    let fields = document.querySelectorAll('input');
-    let fieldsValues = [].slice.call(fields);
-    fieldsValues.forEach(element => {
-        data.push({
-            id: element.id,
-            value: element.value
-        });
-    });
-    data = data.filter(item => item.value.length>0)
-    if(data.length != fieldsValues.length){
-        alert("favor preencher todos os campos");
-    }else{
-        data.forEach(item=> validated[item.id] = item.value)
-        if(validated['password1'] && validated['password2'] && validated['password1'] == validated['password2']){
-            validated['password'] = validated['password1']
-            return validated;
-        }
-        return null;
-    }
-};
 
 const registerDonator = () => {
     let data = {
         name : $('#name')[0].value,
-        password : $('#password')[0].value,
+        password : $('#password1')[0].value,
+        password2 : $('#password2')[0].value,
         email : $('#email')[0].value,
-        dateOfBirth: $('#dateOfBirth')[0].value,
+        // dateOfBirth: $('#dateOfBirth')[0].value,
         bloodType : $('#bloodType')[0].value,
-        genre : $('#genre')[0].value
+        genre : $('input[name=genre]:checked')[0].id || null
     };
     if(
         data.name != "" && 
         data.password != "" && 
+        data.password2 != "" &&
         data.email != "" && 
-        data.dateOfBirth != "" && 
+        // data.dateOfBirth != "" && 
         data.bloodType != "" && 
         data.genre != ""
     ){
-        //Chama Post
+        let url = urlBase + "donators";
+        let options = {
+            body: JSON.stringify(data),
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            }
+        };
+        post(url, options)
+        .then(response => {
+            $('#name')[0].value = "";
+            $('#password1')[0].value = "";
+            $('#password2')[0].value = "";
+            $('#email')[0].value = "";
+            // $('#dateOfBirth')[0].value = "";
+            $('#bloodType')[0].value = "";
+            
+            alert("VOCÊ FOI CADASTRADO COM SUCESSO!");
+        })
+        .catch(err => {
+            console.log(err);
+        })
     }
 };
 
 const registerBank = () => {
+    //name, email, cnpj, phone1, phone2, street,password1, password1
     let data = {
         name : $('#name')[0].value,
-        password : $('#password')[0].value,
+        password : $('#password1')[0].value,
         email : $('#email')[0].value,
-        cnpj: $('#dateOfBirth')[0].value,
-        phones : $('#bloodType')[0].value,
-        address : $('#genre')[0].value
+        cnpj: $('#cnpj')[0].value,
+        phone1 : $('#phone1')[0].value,
+        phone2 : $('#phone2')[0].value,
+        address : $('#street')[0].value,
     };
     if(
         data.name != "" && 
         data.password != "" && 
         data.email != "" && 
         data.cnpj != "" && 
-        data.phones != "" && 
+        data.phone1 != "" && 
         data.address != ""
     ){
-        //Chama Post
+        data.phones = [data.phone1, data.phone2];
+        let url = urlBase + "banks";
+        let options = {
+            body: JSON.stringify(data),
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            }
+        };
+        post(url, options)
+        .then( (response) => {
+            $('#name')[0].value = "";
+            $('#password1')[0].value = "";
+            $('#email')[0].value = "";
+            $('#cnpj')[0].value = "";
+            $('#phone1')[0].value = "";
+            $('#phone2')[0].value = "";
+            $('#street')[0].value = "";
+            
+            alert("Cadastrado com sucesso");
+        })
+        .catch(
+            (err) => console.log(err)
+        )
     }
 };
 
-const login = (who) => {
-    let url = urlBase + who + "/login";
-    let data = {};
-    data['email'] = $('#email')[0].value;
-    data['password'] = $('#password')[0].value;
-    let options = {
-        body: JSON.stringify(data),
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        }
-    };
-    if(data != null) {
+const login = () => {
+    if(! $('input[name=who]:checked')[0].id ) {
+        alert("favor selecionar se é doador ou hospital")
+    }else{
+        let url = urlBase + $('input[name=who]:checked')[0].id + "/login";
+        let data = {};
+        data['email'] = $('#email')[0].value;
+        data['password'] = $('#password')[0].value;
+        let options = {
+            body: JSON.stringify(data),
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            }
+        };
         post(url, options)
-        .then(data => {
-            console.log(data)
-            window.localStorage.setItem("login", JSON.stringify(data))
+        .then(response => {
+            if(!response) {
+                alert("erro ao logar, favor informe suas credenciais corretamente")
+            }else {
+                alert("você foi logado com sucesso, estamos contruindo seu dashboard!")
+                window.localStorage.setItem("login", JSON.stringify(response))
+            }
         })
         .catch(err => console.log("err ", err))
-    };
+    }
 };
